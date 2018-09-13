ecfdf75dc62076f01c81125c75527297da055d41
UmerF <umer.farooq@platalytics.com>
2018-09-13 15:48:05 +0500

    fac query

--- src/main/scala/test/test/RmDump.scala
+++ src/main/scala/test/test/RmDump.scala
@@ -30,12 +30,21 @@ object RmDump {
 
     val facilityQuery = s"""
           SELECT ss1.facility_cd, ss1.site_num 
-          FROM  s_site.study_site ss1 , s_site.study_site ss2
-          WHERE ss1.study_id in ('148') AND 
-          ss1.facility_cd = ss2.facility_cd AND 
-          ss1.study_id = ss2.study_id AND 
-          ss2.site_num in ('216')
-      """
+          FROM  s_site.study_site as ss1 , s_site.study_site as ss2"""
+//          WHERE ss1.study_id in ('148') AND 
+//          ss1.facility_cd = ss2.facility_cd AND 
+//          ss1.study_id = ss2.study_id AND 
+//          ss2.site_num in ('216')"""
+    
+    
+//    val facilityQuery = s"""
+//          SELECT ss1.facility_cd, ss1.site_num 
+//          FROM  s_site.study_site as ss1 , s_site.study_site as ss2
+//          WHERE ss1.study_id in ('148') AND 
+//          ss1.facility_cd = ss2.facility_cd AND 
+//          ss1.study_id = ss2.study_id AND 
+//          ss2.site_num in ('216')
+//      """
 
     var facMap = scala.collection.immutable.Map.empty[String, String]
     val facilityDf = sparkSession.sqlContext.read.jdbc(url, facilityQuery, prop).rdd.
