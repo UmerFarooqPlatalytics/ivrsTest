85402ad2a8d12302d7a73f013bd6ef159df413b6
UmerF <umer.farooq@platalytics.com>
2018-09-24 19:01:15 +0500

    prod creds

--- src/main/scala/test/test/RmDump.scala
+++ src/main/scala/test/test/RmDump.scala
@@ -121,7 +121,7 @@ object RmDump {
       
       ON (
       ('${record.getAs[String]("IVRS_PROJECT_ID")}', '${record.getAs[String]("IVRS_PROTOCOL_NUMBER")}', '${record.getAs[String]("IVRS_PATIENT_ID")}', '${record.getAs[String]("IVRS_COUNTRY")}')
-      IN (SELECT IVRS_PROJECT_ID, IVRS_PROTOCOL_NUMBER, IVRS_PATIENT_ID, IVRS_COUNTRY from S_ACUTRACK.IVRS_ACURIAN_OUTPUT)
+      IN (SELECT t.IVRS_PROJECT_ID, t.IVRS_PROTOCOL_NUMBER, t.IVRS_PATIENT_ID, t.IVRS_COUNTRY from S_ACUTRACK.IVRS_ACURIAN_OUTPUT t)
       )
           
       WHEN MATCHED THEN
